56dfa8137e78f3680a68f131605e83b4557c4a14
Patrick Lecuyer <plecuyer@google.com>
2017-10-05 14:57:45 -0400

    initial demo code commit

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
-package external_api_service
+package main
+
+import (
+	"log"
+	"net"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	pb "source.developers.google.com/p/gaming-backend-demo/r/gaming-backend-demo/proto"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement helloworld.GreeterServer.
+type server struct{}
+
+
